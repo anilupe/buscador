3b6328ac98bd0f5301a1bf4f8b844f4a2a392375
anilupe <ani_pereira@hotmail.es>
2021-11-05 18:24:52 -0500

    creacion vista listview e item

--- app/src/main/java/cab/smart/buscador_canciones/MainActivity.java
+++ app/src/main/java/cab/smart/buscador_canciones/MainActivity.java
@@ -1,14 +1,29 @@
 package cab.smart.buscador_canciones;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import cab.smart.buscador_canciones.commons.commons;
+import cab.smart.buscador_canciones.repositories.RestApi;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    Retrofit.Builder builder=new Retrofit.Builder()
+            .baseUrl(commons.baseURL)
+            .addConverterFactory(GsonConverterFactory.create());
+    Retrofit retrofit =builder.build();
+    RestApi userSongs =retrofit.create(RestApi.class);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView rv;
+
     }
 }

e4e82b18beedb3434c1656e4258539ba1f90bd1a
anilupe <ani_pereira@hotmail.es>
2021-11-05 23:43:22 -0500

    creacion variables globales

--- app/src/main/java/cab/smart/buscador_canciones/commons/commons.java
+++ app/src/main/java/cab/smart/buscador_canciones/commons/commons.java
@@ -3,5 +3,8 @@ package cab.smart.buscador_canciones.commons;
 public class commons {
 //url base para las busquedas
     public static final String baseURL="https://itunes.apple.com/";
+    public static final String mediaType="music";
+    public static final int limit=20;
+    public static final int offset=1;
 
 }

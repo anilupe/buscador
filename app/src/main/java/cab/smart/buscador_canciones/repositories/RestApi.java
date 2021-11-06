b023b6a332ba7a8d27e2d53312ae54afdc8940b7
anilupe <ani_pereira@hotmail.es>
2021-11-05 23:39:21 -0500

    creacion restAPI para retrofit

--- app/src/main/java/cab/smart/buscador_canciones/repositories/RestApi.java
+++ app/src/main/java/cab/smart/buscador_canciones/repositories/RestApi.java
@@ -6,8 +6,12 @@ import java.util.Observable;
 import cab.smart.buscador_canciones.models.SongsModel;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface RestApi {
-    @GET("search?term=in+utero&mediaType=music&limit=20")
-    Call<List<SongsModel>>getSongs();
+    @GET("search")
+    Call<SongsModel>getSongs(@Query("term") String term,
+                             @Query("mediaType") String media,
+                             @Query("limit") int limit,
+                            @Query("offset") int offset);
 }
